eb32bfb82439839fc0e40aba5ffa7f6644a3a683
discoham <chris@yellowlight.org>
2022-07-29 15:12:55 -0700

    Changed Document Title

---
+++ app.js
@@ -0,0 +1,10 @@
+function greeting(phrase){
+    phrase
+    console.log(phrase)
+}
+
+
+let sentence = "Hello Everbody! This is my first argument!"
+
+greeting(sentence)
+greeting("Hello World")
---
+++ example.js
@@ -0,0 +1,9 @@
+function multiply(a, b) {
+
+    return a * b;
+
+}
+
+let x = multiply(5, 6); // Return value will end up in x equals 30
+
+document.write(x);
